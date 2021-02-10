0015c4bc9f25b47c1266a09169f64cc005a152fb
Cameron Harper <whatiloginwithch@gmail.com>
2021-02-10 23:55:44 +0000

    playing about with rust for the first time

---
+++ README.md
@@ -0,0 +1,3 @@
+# README #
+
+Following along with [Rust by Example](https://doc.rust-lang.org/rust-by-example/index.html)
---
+++ hello.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello World!");
+
+    println!("LOG: {message}",
+                message="test"
+            );
+}
